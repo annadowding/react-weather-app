fb15be46be5c11c5bed2e4ea2ea726213f769e1a
Anna Dowding <144890388+annadowding@users.noreply.github.com>
2024-01-03 15:46:17 +0000

    Updated files and added basic styling

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
+import Weather from "./Weather.js";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div class="container">
+        <Weather />
+        <footer>
+          This project was coded by{" "}
+          <a href="https://github.com/annadowding">Anna Dowding</a> and is{" "}
+          <a href="https://github.com/annadowding/weatherapp"> open-sourced</a>.
+        </footer>
+      </div>
     </div>
   );
 }
