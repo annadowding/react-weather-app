12f40be6ec1161ca95d22fa45cb98b7af9f2d486
Anna Dowding <144890388+annadowding@users.noreply.github.com>
2024-01-03 15:54:42 +0000

    Added link to footer

--- src/App.js
+++ src/App.js
@@ -10,7 +10,9 @@ function App() {
         <footer>
           This project was coded by{" "}
           <a href="https://github.com/annadowding">Anna Dowding</a> and is{" "}
-          <a href="https://github.com/annadowding/weatherapp"> open-sourced</a>.
+          <a href="https://github.com/annadowding/weatherapp"> open-sourced</a>
+          .and hosted on{" "}
+          <a href="https://stellar-salmiakki-03ea04.netlify.app/">Netlify</a>
         </footer>
       </div>
     </div>
